aebb50103dd8d56b9f78f115fca48fafb9269767
Ariel Li <xeayerli@gmail.com>
2024-11-06 10:51:38 -0800

    changed out images, changed pres path, methodized

--- day2.py
+++ day2.py
@@ -9,98 +9,15 @@ from present import present_instruction
 import psychopy.gui as psygui
 import datetime
 
-PRACMODE = False
+def pres_img(win, mainimg, imgtype):
 
-#Need to ask ariel how we want to format input of images so we can stay consistent for both day1 and day2
-
-#give the name of the directory with all of the image directories
-
-mainPath = Path(r"day2")
-
-#List which images are the main images by inputing the main image directory 
-
-mainimgdirs = ["negative", "neutral"]
-
-#List which images are the foil images by inputing the foil image directory names
-
-foilimgdirs = ["negative_foil", "neutral_foil"]
-
-#Where do we want to output results
-result_export_dir = "results"
-
-mainpres = []
-
-#How many breaks are in out study
-breaks = 1
-
-#Image size presented
-imgSize = (600, 600)
-
-#Get participant ID
-participant = {"Participant #": ""}
-pnum = psygui.DlgFromDict(participant)
-partnum = participant['Participant #']
-
-#Get previous presentation details
-cur_path = os.path.dirname(__name__)
-
-new_path = os.path.relpath(fr'..\day1\results\P{partnum}\day1pres', cur_path)
-day1pres = pd.read_csv(new_path)
+    c = Clock()
 
 
+    correct = 0
+    rt = 0
+    intrusionnum = 0
 
-#Normalizes and assigns images with proper identifiers
-for imgdir in mainimgdirs:
-    dirpath = os.path.join(mainPath, imgdir)
-    for imgp in os.listdir(dirpath):
-        if imgp != ".DS_Store": #this line filters out the DS_Store file
-            img = Image.open(os.path.join(dirpath, imgp))
-            img = img.resize(imgSize)
-            imgp = os.path.splitext(imgp)[0]
-            row =  day1pres.loc[day1pres['name']==imgp][['val', 'pres']].values[0]
-            val = row[0]
-            pres = row[1]
-            mainpres.append([img, 1, imgp, val, pres])
-
-#Same thing but with foil images
-for imgdir in foilimgdirs:
-    dirpath = os.path.join(mainPath, imgdir)
-    for imgp in os.listdir(dirpath):
-        if imgp != ".DS_Store":
-            img = Image.open(os.path.join(dirpath, imgp))
-            img = img.resize(imgSize)
-            mainpres.append([img, 9, imgp, "NA", "NA"])
-
-#Shuffle for randomness
-random.shuffle(mainpres)
-#initializes rsults data
-df = pd.DataFrame()
-
-correct = False
-c = Clock()
-
-#Present instructions
-win = visual.Window(fullscr=True, units="pix", color="white")
-present_instruction(win, r"Ver2__PTSD_pilot_text\begininstr1.jpg")
-present_instruction(win, r"Ver2__PTSD_pilot_text\begininstr2.jpg")
-
-img_count = 0
-
-#Start presenting
-for imgpair in mainpres:
-
-    #Checks to see if we present break
-    if img_count == int(len(mainpres) / (breaks + 1)) == 0:
-        present_instruction(win, r"Ver2__PTSD_pilot_text\break.jpg")
-        intcount = 0
-
-    mainimg = imgpair[0]
-    imgtype = imgpair[1]
-
-    # Set up the window
-    
-
-    # Load the images
     text = visual.TextStim(win, "Have you been presented this image before?", pos=(0, 400), color="black", font='arial', height=50)
     image1 = visual.ImageStim(win, image=mainimg, pos=(0, 0))
     text1 = visual.TextStim(win, "Yes (1)", pos=(-400, -400), color="black", font='arial', height=35)
@@ -118,7 +35,6 @@ for imgpair in mainpres:
     while True:
 
         intrusionnum = "0"
-        endt = c.getTime()
         keys = event.waitKeys()[0]
         
         rt = c.getTime()
@@ -169,7 +85,104 @@ for imgpair in mainpres:
             text1.draw()
             text2.draw()
             win.flip()
-            
+    return correct, rt, intrusionnum
+
+PRACMODE = False
+
+#Need to ask ariel how we want to format input of images so we can stay consistent for both day1 and day2
+
+#give the name of the directory with all of the image directories
+
+mainPath = Path(r"day2")
+
+#List which images are the main images by inputing the main image directory 
+
+mainimgdirs = ["negative", "neutral"]
+
+#List which images are the foil images by inputing the foil image directory names
+
+foilimgdirs = ["negative_foil", "neutral_foil"]
+
+#Where do we want to output results
+result_export_dir = "results"
+
+mainpres = []
+
+#How many breaks are in out study
+breaks = 1
+
+#Image size presented
+imgSize = (600, 600)
+
+#Get participant ID
+participant = {"Participant #": ""}
+pnum = psygui.DlgFromDict(participant)
+partnum = participant['Participant #']
+
+#Get previous presentation details
+cur_path = os.path.dirname(__name__)
+
+new_path = os.path.relpath(fr'..\PTSDstudyday1\results\P{partnum}\day1pres', cur_path)
+day1pres = pd.read_csv(new_path)
+
+
+
+#Normalizes and assigns images with proper identifiers
+for imgdir in mainimgdirs:
+    dirpath = os.path.join(mainPath, imgdir)
+    for imgp in os.listdir(dirpath):
+        if imgp != ".DS_Store": #this line filters out the DS_Store file
+            img = Image.open(os.path.join(dirpath, imgp))
+            img = img.resize(imgSize)
+            imgp = os.path.splitext(imgp)[0]
+            row =  day1pres.loc[day1pres['name']==imgp][['val', 'pres']].values[0]
+            val = row[0]
+            pres = row[1]
+            mainpres.append([img, 1, imgp, val, pres])
+
+#Same thing but with foil images
+for imgdir in foilimgdirs:
+    dirpath = os.path.join(mainPath, imgdir)
+    for imgp in os.listdir(dirpath):
+        if imgp != ".DS_Store":
+            img = Image.open(os.path.join(dirpath, imgp))
+            img = img.resize(imgSize)
+            mainpres.append([img, 9, imgp, "NA", "NA"])
+
+#Shuffle for randomness
+random.shuffle(mainpres)
+#initializes rsults data
+df = pd.DataFrame()
+
+#Present instructions
+win = visual.Window(fullscr=True, units="pix", color="white")
+present_instruction(win, r"Ver2__PTSD_pilot_text\begininstr1.jpg")
+present_instruction(win, r"Ver2__PTSD_pilot_text\begininstr2.jpg")
+
+img_count = 0
+
+for img in os.listdir("pracsource"):
+    pres_img(img, 1) #"img" should be replaced with image path, "evil" a placeholder for image valence
+
+present_instruction(win, r"Ver2__PTSD_pilot_text\prac2.jpg") #r ignores slash; replace this with relative path
+
+#Start presenting
+for imgpair in mainpres:
+
+    #Checks to see if we present break
+    if img_count == int(len(mainpres) / (breaks + 1)) == 0:
+        present_instruction(win, r"Ver2__PTSD_pilot_text\break.jpg")
+        intcount = 0
+
+    mainimg = imgpair[0]
+    imgtype = imgpair[1]
+
+    # Set up the window
+    
+    correct, rt, intrusionnum = pres_img(win, mainimg, imgtype)
+
+    # Load the images
+  
     #Save image response
     df = pd.concat([df, pd.DataFrame({"participant": partnum, "img":imgpair[2], "val":imgpair[3], "pres":imgpair[4], "correct":correct,"rt":rt, "intrusions":intrusionnum
                                       ,"date" : datetime.datetime.now()}, index=[0])], ignore_index=True)
@@ -188,6 +201,4 @@ if(not PRACMODE):
 # Clean up
 win.close()
 core.quit()
-
-
 
